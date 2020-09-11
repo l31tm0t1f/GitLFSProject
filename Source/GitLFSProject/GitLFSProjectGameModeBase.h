b7c281aba5fc4846b2e286590f0149654ac03988
l31tm0t1f <siimsoonsein@gmail.com>
2020-09-11 13:45:58 +0300

    test_01

---
+++ Source/GitLFSProject/GitLFSProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GitLFSProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITLFSPROJECT_API AGitLFSProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
